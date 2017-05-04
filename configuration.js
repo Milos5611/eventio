1fc7d3eb350770622512d214a1921180a4f67f1c
Milos Nikolic <milos.nikolic@prodyna.com>
2017-05-04 13:32:46 +0200

    Login

--- configuration.js
+++ configuration.js
@@ -1,6 +1,5 @@
-window.de = window.de || {};
-window.de.daad = window.de.daad || {};
-window.de.daad.monitoring = window.de.daad.monitoring || {
+window.strv = window.strv || {};
+window.strv.eventio = window.strv.eventio || {
     "BASE_URL": "https://testproject-api-v2.strv.com",
     "SHOW_TEST_HINT": false,
     "SHOW_DEV_HINT": false
