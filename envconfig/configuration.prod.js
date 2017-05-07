46477bf6822f994a1247e4553d3a65981bb834b8
Milos Nikolic <milos.nikolic@prodyna.com>
2017-05-07 14:45:08 +0200

    Heroku

--- envconfig/configuration.prod.js
+++ envconfig/configuration.prod.js
@@ -1,6 +1,6 @@
 window.strv = window.strv || {};
 window.strv.eventio = window.strv.eventio || {
-		"BASE_URL": "https://private-anon-8fcbd93571-strvtestprojectv2.apiary-mock.com",
+		"BASE_URL": "https://testproject-api-v2.strv.com",
 		"SHOW_TEST_HINT": false,
 		"SHOW_DEV_HINT": false
 	};
