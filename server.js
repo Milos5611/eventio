d9ad886f9e9583320f14076fd80d9e9096986454
Milos Nikolic <milos.nikolic@prodyna.com>
2017-05-07 16:39:10 +0200

    Fixing problem on heroku

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require("express"),
 	port = process.env.PORT || 8080,
 	app = express();
 
-app.use(express.static(__dirname + "/build"));
+app.use(express.static(__dirname + "./build"));
 app.get("*", (req, res) => {
-	res.sendFile(path.resolve(__dirname, "/build", "index.html"))
+	res.sendFile(path.resolve(__dirname, "./build", "index.html"))
 });
 app.listen(port);
 console.log("Server started");
